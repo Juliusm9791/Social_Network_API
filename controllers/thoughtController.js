07d4b3fe2f2027cac9d77d399c066beef0e36ce6
Julius Markauskas <juliusm9791@gmail.com>
2022-01-06 21:44:16 -0500

    cleaned code thoughtController file

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -49,18 +49,9 @@ module.exports = {
           res.status(500).json(err);
         });
     },
-  // Delete a thought and remove them from the course
+  // Delete a thought 
   deleteThought(req, res) {
     Thought.findOneAndRemove({ _id: req.params.thoughtId })
-      // .then((user) =>
-      //   !user
-      //     ? res.status(404).json({ message: 'No such user exists' })
-      //     : Course.findOneAndUpdate(
-      //         { students: req.params.studentId },
-      //         { $pull: { students: req.params.studentId } },
-      //         { new: true }
-      //       )
-      // )
       .then((thought) =>
         !thought
           ? res.status(404).json({
@@ -74,38 +65,4 @@ module.exports = {
       });
   },
 
-  // // Add an assignment to a student
-  // addAssignment(req, res) {
-  //   console.log('You are adding an assignment');
-  //   console.log(req.body);
-  //   Student.findOneAndUpdate(
-  //     { _id: req.params.studentId },
-  //     { $addToSet: { assignments: req.body } },
-  //     { runValidators: true, new: true }
-  //   )
-  //     .then((student) =>
-  //       !student
-  //         ? res
-  //             .status(404)
-  //             .json({ message: 'No student found with that ID :(' })
-  //         : res.json(student)
-  //     )
-  //     .catch((err) => res.status(500).json(err));
-  // },
-  // // Remove assignment from a student
-  // removeAssignment(req, res) {
-  //   Student.findOneAndUpdate(
-  //     { _id: req.params.studentId },
-  //     { $pull: { assignment: { assignmentId: req.params.assignmentId } } },
-  //     { runValidators: true, new: true }
-  //   )
-  //     .then((student) =>
-  //       !student
-  //         ? res
-  //             .status(404)
-  //             .json({ message: 'No student found with that ID :(' })
-  //         : res.json(student)
-  //     )
-  //     .catch((err) => res.status(500).json(err));
-  // },
 };
